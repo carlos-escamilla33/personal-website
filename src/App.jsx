f030e17acf9a6bb7a8dc1caf2ad54d39e7fa324a
Carlos Escamilla <carlosaescamilla3@gmail.com>
2025-09-23 11:28:03 -0700

    working on header and navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { Header } from "./components"
 
 function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
+      <Header />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
-import {useState} = from "react";
+// import {useState} from "react";
 
 const Navbar = () => {
     const navItems = ["Home", "About", "Projects", "Contact"];
-    const [isOpen, setIsOpen] = useState(false);
+    // const [isOpen, setIsOpen] = useState(false);
 
     return(
-        <nav className="hidden md:flex space-x-8">
+        <nav className="relative">
             {
                 navItems.map((item) => {
-                    <a key={item} href={`#${item.toLowerCase()}`}>
-                        {item}
-                    </a>
+                    return <a key={item}
+                     className="text-gray-600 hover:text-gray-800 font medium transition-colors"
+                     href="">{item}</a>
                 })
             }
         </nav>
